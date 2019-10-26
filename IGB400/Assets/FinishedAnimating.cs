f74584d1473d44b96d9ff315669ff2db5aa80638
Tom Hawkins <tks.hawkins11@gmail.com>
2019-10-26 13:06:33 +1000

    Stuff

---
+++ IGB400/Assets/FinishedAnimating.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishedAnimating : MonoBehaviour
+{
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Done"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ IGB400/Assets/FloatEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatEffect : MonoBehaviour
+{
+    
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+ 
+
+    Vector2 posOffset = new Vector2 ();
+    Vector2 tempPos = new Vector2 ();
+ 
+    // Use this for initialization
+    void Start () {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+     
+    // Update is called once per frame
+    void Update () {
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
+ 
+        transform.position = tempPos;
+    }
+}
